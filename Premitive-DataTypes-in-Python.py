7c4ec2f727338591b09a9c0aa2e45dc1e8b19de0
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-23 22:52:50 +0500

    Premitive-DataTypes-in-Python.py

--- Premitive-DataTypes-in-Python.py
+++ Premitive-DataTypes-in-Python.py
@@ -1,1 +1,23 @@
-print("Hello World")
+# Integer
+num_int = 42
+
+# Float
+num_float = 3.14
+
+# Character (Python does not have a separate character data type, strings are used instead)
+my_char = 'A'
+
+# Boolean
+is_true = True
+is_false = False
+
+
+# Printing the values and their types
+print("Integer:", num_int, type(num_int))
+
+print("Float:", num_float, type(num_float))
+
+print("Character:", my_char, type(my_char))
+
+print("Boolean (True):", is_true, type(is_true))
+print("Boolean (False):", is_false, type(is_false))
