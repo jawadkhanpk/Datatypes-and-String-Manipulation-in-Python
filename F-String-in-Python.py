28ef6c7531a9b1689cb6a04a361b22828c5ed6f2
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-24 21:40:30 +0500

    F-String in Python

---
+++ F-String-in-Python.py
@@ -0,0 +1,8 @@
+# F-String provide a concise and convenient way to embed expressions inside strings for formatting purposes.
+
+score = 0
+height = 1.8
+isWinning = True
+
+# F-String
+print(f"The score is {score}, the heigt is {height}, and your are winning is {isWinning}")
